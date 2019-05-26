a452a6c9841f5a4e3bc313ea14f59859a5e545d2
Rodrigo Souza <alves.wm@gmail.com>
2019-05-26 16:18:36 -0300

    changes config to Global gem (#2)

--- config/initializers/database.rb
+++ config/initializers/database.rb
@@ -1,3 +1,2 @@
-db_config = $meta_config.parse('database.yml')[ENV['RACK_ENV']]
 ActiveRecord::Base.default_timezone = :utc
-OTR::ActiveRecord.configure_from_hash!(db_config)
+OTR::ActiveRecord.configure_from_hash!(Global.database.to_hash)
